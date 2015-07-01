55b62bf1062501106f6a1d30960455f0c5ddd6b8
umbobabo <umbertobabini@economist.com>
2015-07-01 17:41:26 +0100

    Added arrows icons

--- example.js
+++ example.js
@@ -7,8 +7,13 @@ export default (
     <Icon type='twitter' fill='#FFCC00' />
     <Icon type='googleplus' fill='#000000' />
     <Icon type='linkedin' fill='#AABBCC' />
-    <Icon type='whatsapp' fill='#000000' />
+    <Icon type='whatsapp' fill='#FFFFFF' />
     <Icon type='mail' fill='#000000' />
     <Icon type='rss' fill='#000000' />
+    <Icon type='down' fill='#000000' />
+    <Icon type='up' fill='#000000' />
+    <Icon type='right' fill='#000000' />
+    <Icon type='left' fill='#000000' />
+    <Icon type='logoWorldIF' fill='#000000' />
   </div>
 );

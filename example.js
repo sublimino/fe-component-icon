0bba547cb0e8ff1cbeca593369e514984956060a
umbobabo <umbertobabini@economist.com>
2015-07-01 16:37:28 +0100

    Initial set of icons

---
+++ example.js
@@ -0,0 +1,14 @@
+import Icon from './index';
+import React from 'react';
+
+export default (
+  <div>
+    <Icon type='facebook' fill='#000000' />
+    <Icon type='twitter' fill='#FFCC00' />
+    <Icon type='googleplus' fill='#000000' />
+    <Icon type='linkedin' fill='#AABBCC' />
+    <Icon type='whatsapp' fill='#000000' />
+    <Icon type='mail' fill='#000000' />
+    <Icon type='rss' fill='#000000' />
+  </div>
+);

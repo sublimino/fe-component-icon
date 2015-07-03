2940581b3003f671dfae0a143b41fc4f1dca3a11
umbobabo <umbertobabini@economist.com>
2015-07-03 16:00:12 +0100

    Updated icons, optimised code, added shape props

--- example.js
+++ example.js
@@ -3,17 +3,39 @@ import React from 'react';
 
 export default (
   <div>
-    <Icon type='facebook' fill='#000000' />
-    <Icon type='twitter' fill='#FFCC00' />
-    <Icon type='googleplus' fill='#000000' />
-    <Icon type='linkedin' fill='#AABBCC' />
-    <Icon type='whatsapp' fill='#FFFFFF' />
-    <Icon type='mail' fill='#000000' />
-    <Icon type='rss' fill='#000000' />
-    <Icon type='down' fill='#000000' />
-    <Icon type='up' fill='#000000' />
-    <Icon type='right' fill='#000000' />
-    <Icon type='left' fill='#000000' />
-    <Icon type='logoWorldIF' fill='#000000' />
+    <h1>Default values</h1>
+    <Icon type='facebook' />
+    <Icon type='twitter' />
+    <Icon type='googleplus' />
+    <Icon type='linkedin' />
+    <Icon type='whatsapp' />
+    <Icon type='mail' />
+    <Icon type='rss' />
+    <Icon type='down' />
+    <Icon type='up' />
+    <Icon type='right' />
+    <Icon type='left' />
+    <Icon type='logoWorldIF' />
+    <Icon type='home' />
+    <Icon type='hamburger' />
+    <Icon type='user' />
+    <Icon type='magnifier' />
+    <h1>Overwrite</h1>
+    <Icon type='facebook' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='twitter' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='googleplus' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='linkedin' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='whatsapp' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='mail' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='rss' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='down' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='up' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='right' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='left' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='logoWorldIF' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='home' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='hamburger' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='user' color='#BBFFCC' background="red" shape="square" />
+    <Icon type='magnifier' color='#BBFFCC' background="red" shape="square" />
   </div>
 );
